d5cd8746fd7ee8595f7428500b42ed1cf7416358
Rendy Arya Kemal <renrror@gmail.com>
2024-07-06 21:30:19 +0700

    feat: add dev server

--- likulau/console.py
+++ likulau/console.py
@@ -11,10 +11,25 @@ app = typer.Typer()
 
 @app.command()
 def run():
-    logger.info("Initialize app")
-    app = create_app()
+    uvicorn.run(
+        "likulau.app:create_app",
+        port=3000,
+        log_level="info",
+        factory=True,
+        lifespan="on",
+    )
 
-    uvicorn.run(app, port=3000, log_level="info")
+
+@app.command()
+def dev():
+    uvicorn.run(
+        "likulau.app:create_app",
+        port=3000,
+        log_level="info",
+        reload=True,
+        factory=True,
+        lifespan="on",
+    )
 
 
 @app.command()
