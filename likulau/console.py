15fd2089c403440db8e4a3797089294a65865c07
Rendy Arya Kemal <renrror@gmail.com>
2024-07-07 16:48:42 +0700

    feat: add uvicorn options

--- likulau/console.py
+++ likulau/console.py
@@ -1,32 +1,83 @@
 import logging
+from typing import Annotated
 
 import typer
 import uvicorn
-
-from likulau.app import create_app
 
 logger = logging.getLogger("likulau.console")
 app = typer.Typer()
 
 
 @app.command()
-def run():
+def run(
+    host: Annotated[
+        str,
+        typer.Option(help="The host to serve on."),
+    ] = "127.0.0.1",
+    port: Annotated[
+        int,
+        typer.Option(help="The port to serve on."),
+    ] = 8000,
+    reload: Annotated[
+        bool,
+        typer.Option(help="Enable auto-reload of the server when files change."),
+    ] = True,
+    root_path: Annotated[
+        str,
+        typer.Option(
+            help="Tell your app that it is being served to the outside world with some path prefix set up in some termination proxy or similar."
+        ),
+    ] = "",
+    proxy_headers: Annotated[
+        bool,
+        typer.Option(
+            help="Enable/Disable X-Forwarded-Proto, X-Forwarded-For, X-Forwarded-Port to populate remote address info."
+        ),
+    ] = True,
+):
     uvicorn.run(
         "likulau.app:create_app",
-        port=3000,
+        host=host,
+        port=port,
         log_level="info",
+        reload=reload,
+        root_path=root_path,
+        proxy_headers=proxy_headers,
         factory=True,
         lifespan="on",
     )
 
 
 @app.command()
-def dev():
+def dev(
+    host: Annotated[
+        str,
+        typer.Option(help="The host to serve on."),
+    ] = "127.0.0.1",
+    port: Annotated[
+        int,
+        typer.Option(help="The port to serve on."),
+    ] = 8000,
+    root_path: Annotated[
+        str,
+        typer.Option(
+            help="Tell your app that it is being served to the outside world with some path prefix set up in some termination proxy or similar."
+        ),
+    ] = "",
+    proxy_headers: Annotated[
+        bool,
+        typer.Option(
+            help="Enable/Disable X-Forwarded-Proto, X-Forwarded-For, X-Forwarded-Port to populate remote address info."
+        ),
+    ] = True,
+):
     uvicorn.run(
         "likulau.app:create_app",
-        port=3000,
+        host=host,
+        port=port,
         log_level="info",
-        reload=True,
+        root_path=root_path,
+        proxy_headers=proxy_headers,
         factory=True,
         lifespan="on",
     )
@@ -34,7 +85,7 @@ def dev():
 
 @app.command()
 def build():
-    print("TODO")
+    raise NotImplementedError()
 
 
 if __name__ == "__main__":
